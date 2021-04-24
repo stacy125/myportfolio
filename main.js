441dea573a1d4f09359591d3eb1577c9689efe6e
stacy leitstein <unique523@yahoo.com>
2021-04-24 13:16:38 -0400

    fially working front page

---
+++ main.js
@@ -0,0 +1,211 @@
+import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js"
+import * as dat from 'dat.GUI'
+import gsap from 'gsap'
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
+
+
+console.log(dat);
+
+const raycaster = new THREE.Raycaster()
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+    75,
+    innerWidth / innerHeight,
+    0.1,
+    1000
+)
+const renderer = new THREE.WebGLRenderer()
+
+
+renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
+
+function generatePlane() {
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(
+        world.plane.width,
+        world.plane.height,
+        world.plane.widthSegments,
+        world.plane.heightSegments
+    )
+
+    // vertices position randomization
+    const { array } = planeMesh.geometry.attributes.position
+    const randomValues = []
+    for (let i = 0; i < array.length; i++) {
+        if (i % 3 === 0) {
+            const x = array[i]
+            const y = array[i + 1]
+            const z = array[i + 2]
+
+            array[i] = x + (Math.random() - 0.5) * 3
+            array[i + 1] = y + (Math.random() - 0.5) * 3
+            array[i + 2] = z + Math.random() * 3
+        }
+        randomValues.push(Math.random() * Math.PI * 2)
+    }
+
+    planeMesh.geometry.attributes.position.randomValues = randomValues
+    planeMesh.geometry.attributes.position.originalPosition =
+        planeMesh.geometry.attributes.position.array
+
+    // color attribute addition
+    const colors = []
+    for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+        colors.push(0, 0.19, 0.4)
+    }
+
+    planeMesh.geometry.setAttribute('color',
+        new THREE.BufferAttribute(new Float32Array(colors), 3)
+    )
+}
+
+const gui = new dat.GUI()
+const world = {
+    plane: {
+        width: 400,
+        height: 400,
+        widthSegments: 50,
+        heightSegments: 50
+    }
+}
+
+gui.add(world.plane, 'width', 1, 500).onChange(generatePlane)
+gui.add(world.plane, 'height', 1, 500).onChange(generatePlane)
+gui.add(world.plane, 'widthSegments', 1, 100).onChange(generatePlane)
+gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane)
+
+document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement)
+// const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
+// const mesh = new THREE.Mesh(boxGeometry, material)
+// scene.add(mesh)
+
+camera.position.z = 50
+
+const planeGeometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments)
+
+const planeMaterial = new THREE.MeshPhongMaterial({
+    // color: 0xff0000,
+    side: THREE.DoubleSide,
+    flatShading: THREE.FlatShading,
+    vertexColors: true
+})
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+scene.add(planeMesh)
+generatePlane()
+
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, -1, 1)
+scene.add(light)
+
+const backlight = new THREE.DirectionalLight(0xffffff, 1)
+backlight.position.set(0, 0, -1)
+scene.add(backlight)
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+let frame = 0
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    raycaster.setFromCamera(mouse, camera)
+    frame += 0.01
+
+    const {
+        array,
+        originalPosition,
+        randomValues
+    } = planeMesh.geometry.attributes.position
+    for (let i = 0; i < array.length; i += 3) {
+        // x
+        array[i] = originalPosition[i] +
+            Math.cos(frame + randomValues[i]) * 0.01
+
+        // y
+        array[i + 1] = originalPosition[i + 1] +
+            Math.sin(frame + randomValues[i + 1]) * 0.001
+    }
+
+    planeMesh.geometry.attributes.position.needsUpdate = true
+
+    const intersects = raycaster.intersectObject(planeMesh)
+    if (intersects.length > 0) {
+        const { color } = intersects[0].object.geometry.attributes
+
+        // vertices 1
+        color.setX(intersects[0].face.a, 0.1)
+        color.setY(intersects[0].face.b, 0.5)
+        color.setZ(intersects[0].face.b, 1)
+
+        // vertices 2
+        color.setX(intersects[0].face.c, 0.1)
+        color.setY(intersects[0].face.c, 0.5)
+        color.setZ(intersects[0].face.c, 1)
+
+        // vertices 3
+        color.setX(intersects[0].face.c, 0.1)
+        color.setY(intersects[0].face.c, 0.5)
+        color.setZ(intersects[0].face.c, 1)
+
+        color.needsUpdate = true
+        // changes back to original color
+        const initialColor = {
+            r: 0,
+            g: 0.19,
+            b: 0.4
+        }
+
+        const hoverColor = {
+            r: 0.1,
+            g: 0.5,
+            b: 1
+        }
+        gsap.to(hoverColor, {
+            r: initialColor.r,
+            g: initialColor.g,
+            b: initialColor.b,
+            duration: 1,
+            onUpdate: () => {
+                // vertices 1
+                color.setX(intersects[0].face.a, hoverColor.r)
+                color.setY(intersects[0].face.a, hoverColor.g)
+                color.setZ(intersects[0].face.a, hoverColor.b)
+
+                // vertices 2
+                color.setX(intersects[0].face.b, hoverColor.r)
+                color.setY(intersects[0].face.b, hoverColor.g)
+                color.setZ(intersects[0].face.b, hoverColor.b)
+
+                // vertices 3
+                color.setX(intersects[0].face.c, hoverColor.r)
+                color.setY(intersects[0].face.c, hoverColor.g)
+                color.setZ(intersects[0].face.c, hoverColor.b)
+                color.needsUpdate = true
+
+            }
+        })
+    }
+    // mesh.rotation.x += 0.01
+    // mesh.rotation.y += 0.01
+    // planeMesh.rotation.x += 0.01
+
+}
+
+renderer.render(scene, camera)
+
+animate()
+addEventListener('mousemove', (event) => {
+    mouse.x = (event.clientX / innerWidth) * 2 - 1
+    mouse.y = -(event.clientY / innerHeight) * 2 + 1
+})
