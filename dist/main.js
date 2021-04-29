ef75b0e0f91b914a518ddd5e45dbcb5857911cc4
stacy leitstein <unique523@yahoo.com>
2021-04-29 15:20:08 -0400

    fixing errors

--- dist/main.js
+++ dist/main.js
@@ -74,7 +74,7 @@ gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane)
 
 document.body.appendChild(renderer.domElement)
 
-controls = new OrbitControls(camera, renderer.domElement)
+const controls = new THREE.OrbitControls(camera, renderer.domElement)
 // const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 // const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
 // const mesh = new THREE.Mesh(boxGeometry, material)
