e7d87c03e9909e591082d94d15dfbfe2e514637b
stacy leitstein <unique523@yahoo.com>
2021-05-04 22:02:37 -0400

    added log

--- main.js
+++ main.js
@@ -75,6 +75,7 @@ gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+console.log(controls, 'now');
 // const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 // const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
 // const mesh = new THREE.Mesh(boxGeometry, material)
