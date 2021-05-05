d5606834d7ceca51b136ae1eb90450b7388906e9
stacy leitstein <unique523@yahoo.com>
2021-05-04 22:10:15 -0400

    added class describing orbitControls

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+class MapControls extends OrbitControls {
 
+    constructor(object, domElement) {
+
+        super(object, domElement);
+
+        this.screenSpacePanning = false; // pan orthogonal to world-space direction camera.up
+
+        this.mouseButtons.LEFT = MOUSE.PAN;
+        this.mouseButtons.RIGHT = MOUSE.ROTATE;
+
+        this.touches.ONE = TOUCH.PAN;
+        this.touches.TWO = TOUCH.DOLLY_ROTATE;
+
+    }
+
+}
 
 console.log('hello');
 console.log(dat);
